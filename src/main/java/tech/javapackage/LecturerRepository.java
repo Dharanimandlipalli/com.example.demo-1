3863530fb3f82d14a10584c36f0fea4cfda8776e
Dharanimandlipallisettings <dharani.m@beaversin.com>
2021-05-26 18:07:41 +0530

    backendcommit

---
+++ src/main/java/tech/javapackage/LecturerRepository.java
@@ -0,0 +1,16 @@
+package tech.javapackage;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+
+public interface LecturerRepository extends MongoRepository<Lecturer , Integer> {
+
+ 
+
+}
+
+
+
+
+
